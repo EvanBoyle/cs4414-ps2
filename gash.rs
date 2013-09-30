7012e8ab56dfe6daf973d5b2a478724165b8e607
EvanBoyle <epb5te@virginia.edu>
2013-09-29 22:55:22 -0400

    finished rustic bg processes

--- gash.rs
+++ gash.rs
@@ -1,4 +1,4 @@
-use std::{io, run, os, vec};
+use std::{io, run, os, vec, option, libc, task};
 
 fn main() {
     static CMD_PROMPT: &'static str = "gash > ";
@@ -18,14 +18,29 @@ fn main() {
         if argv.len() > 0 {
 		let cmd = argv.connect(" ");
             let program = argv.remove(0);
+		let arglen = argv.len();
 		hist.push(copy cmd);
+		let mut bg = false;
+		if(argv.len()>0){
+			if(argv[argv.len()-1].eq(&~"&")){
+				bg = true;
+				argv.remove(arglen-1);
+			}
+		}
+		let args  : ~[~str] = match(argv.len()){
+			0=>  ~[~""],
+			_ => copy argv
+
+
+		};
 		
-		let opts = run::ProcessOptions::new();
-		/*let opts = run::ProcessOptions {
-            out_fd: std::libc::STDOUT_FILENO,
+		
+		//let opts = run::ProcessOptions::new();
+		let opts = run::ProcessOptions {
+            out_fd: Some(std::libc::STDOUT_FILENO),
             .. run::ProcessOptions::new()
-        };
-	*/
+        }; 
+	
             match program {
                 ~"exit"     => {return; }
                 		//must reflect change in directory to mut cwd ^^^
@@ -41,8 +56,20 @@ fn main() {
 
 
 }
-                _           => {//run::process_status(program, argv);
-				let mut proc = run::Process::new(program, argv, opts);				
+                _           => {
+				//libc::funcs::posix88::unistd::fork();
+				
+				if(bg){
+					println(~"bg");
+					
+				do task::spawn_sched(task::SingleThreaded) { run::process_status(program, args);}	
+				
+				}
+				else {
+					run::process_status(program, argv); 
+
+				}
+				//let mut proc = run::Process::new(program, argv, opts);				
 
 }
             }
